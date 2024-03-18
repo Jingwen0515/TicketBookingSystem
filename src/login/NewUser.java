bc8cd441c7209b0c0ca0b78093b0b85a005fe82d
Jay <jingwen05@gmail.com>
2024-03-18 01:15:52 +0000

    Completed the back button for the register form

--- src/login/NewUser.java
+++ src/login/NewUser.java
@@ -51,7 +51,7 @@ public class NewUser extends javax.swing.JFrame {
         newFirstName = new javax.swing.JTextField();
         newLastName = new javax.swing.JTextField();
         newPhoneNumber = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        BackButton = new javax.swing.JButton();
         RegisterButton = new javax.swing.JButton();
         newPassword = new javax.swing.JPasswordField();
         newReenterPassword = new javax.swing.JPasswordField();
@@ -116,11 +116,11 @@ public class NewUser extends javax.swing.JFrame {
             }
         });
 
-        jButton1.setFont(new java.awt.Font("Helvetica Neue", 1, 18)); // NOI18N
-        jButton1.setText("Back");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        BackButton.setFont(new java.awt.Font("Helvetica Neue", 1, 18)); // NOI18N
+        BackButton.setText("Back");
+        BackButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jButton1ActionPerformed(evt);
+                BackButtonActionPerformed(evt);
             }
         });
 
@@ -174,7 +174,7 @@ public class NewUser extends javax.swing.JFrame {
                                     .addComponent(jLabel7)
                                     .addGroup(jPanel3Layout.createSequentialGroup()
                                         .addGap(8, 8, 8)
-                                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                                        .addComponent(BackButton, javax.swing.GroupLayout.PREFERRED_SIZE, 97, javax.swing.GroupLayout.PREFERRED_SIZE)))
                                 .addGap(71, 71, 71)
                                 .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                                     .addComponent(newFirstName)
@@ -221,7 +221,7 @@ public class NewUser extends javax.swing.JFrame {
                     .addComponent(jLabel6)
                     .addComponent(newPhoneNumber, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 22, Short.MAX_VALUE)
-                .addComponent(jButton1)
+                .addComponent(BackButton)
                 .addContainerGap())
             .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                 .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
@@ -270,9 +270,11 @@ public class NewUser extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_newPhoneNumberActionPerformed
 
-    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
-    }//GEN-LAST:event_jButton1ActionPerformed
+    private void BackButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BackButtonActionPerformed
+        Login_Application login = new Login_Application();
+        login.setVisible(true);
+        dispose();
+    }//GEN-LAST:event_BackButtonActionPerformed
 
     private void RegisterButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_RegisterButtonActionPerformed
         boolean usernameIsTaken = false;
@@ -372,8 +374,8 @@ public class NewUser extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BackButton;
     private javax.swing.JButton RegisterButton;
-    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
